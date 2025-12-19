451c6184c5b6b229e6ef3f9e8e5665d4538c9dcf
Satyam Kumar Pandey <143404527+satyampandey1411@users.noreply.github.com>
2025-12-19 22:11:16 +0530

    Implement symmetric binary tree check

---
+++ Tree/Symmatric_binary_tree.java
@@ -0,0 +1,27 @@
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true; // Both nodes are null, so they are symmetric
+        if (t1 == null || t2 == null || (t1.val != t2.val)) return false; // One node is null, the other isn't
+
+        return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+}
