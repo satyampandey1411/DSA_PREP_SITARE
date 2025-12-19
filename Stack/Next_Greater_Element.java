d7c1e358a305a07755d6e03e4f0f30aacd2f48ec
Satyam Kumar Pandey <143404527+satyampandey1411@users.noreply.github.com>
2025-12-19 13:27:25 +0530

    Create Next_Greater_Element.java

---
+++ Stack/Next_Greater_Element.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> s = new Stack<>();
+
+        for(int i=nums2.length-1; i>=0; i--){
+            int ele = nums2[i];
+            while (!s.isEmpty() && s.peek() < ele) {
+                s.pop();
+            }
+            if(!s.isEmpty()){
+                map.put(ele, s.peek());
+            }
+            s.push(ele);
+        }
+
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            if(map.containsKey(nums1[i])){
+                res[i] = map.get(nums1[i]);
+            }else {
+                res[i] = -1;
+            }
+        }
+        return res;
+    }
+}
